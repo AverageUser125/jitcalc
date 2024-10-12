1682552aaee6511b325d63fc634a56be3b57e7d0
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2024-10-12 23:20:54 +0300

    move jit part to hotkey for testing

--- src/gui/displayPoints.cpp
+++ src/gui/displayPoints.cpp
@@ -1,11 +1,27 @@
 #include "displayPoints.hpp"
 #include "platformInput.h"
 #include "mainGui.hpp"
+#include "parser.hpp"
+#include "JITcompiler.hpp"
 #include <iostream>
 
 bool gameLogic(float deltaTime) {
 	if (platform::isButtonPressedOn(platform::Button::A)) {
-		std::cout << "A";
+		// x ^ (0.5) / (x ^ 2) causes crash (negative x)
+		// x ^ (0.5) / (2 ) doesn't crash
+		std::string input = "x * x";
+
+		// std::getline(std::cin, input);
+		Parser parser(input);
+		ExpressionNode* tree = parser.parserParseExpression(Precedence::MIN);
+		parser.parserDebugDumpTree(tree, 0);
+
+		JITCompiler jit;
+		auto func = jit.compile(tree);
+
+		for (double i = -1; i <= 2; i++) {
+			std::cout << func(i) << '\n';
+		}
 	}
 
 	if (platform::isButtonPressedOn(platform::Button::F11)) {
--- src/main.cpp
+++ src/main.cpp
@@ -20,24 +20,7 @@ int main() {
 	guiCleanup();
 	
 	/*
-	// x ^ (0.5) / (x ^ 2) causes crash (negative x)
-	// x ^ (0.5) / (2 ) doesn't crash
-	std::string input = "x * x";
-
-	// std::getline(std::cin, input);
-	Parser parser(input);
-	ExpressionNode* tree = parser.parserParseExpression(Precedence::MIN);
-	parser.parserDebugDumpTree(tree, 0);
-
-	JITCompiler jit;
-	auto func = jit.compile(tree);
-
-
-	for (double i = -1; i <= 2; i++) {
-		std::cout << func(i) << '\n';
-	}
 	
-
 	return EXIT_SUCCESS;
 	*/
 }
