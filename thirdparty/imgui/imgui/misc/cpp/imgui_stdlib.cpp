be7aacdef1faf9ca00511ed4c2956a251a46c5ae
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2024-10-14 21:23:00 +0300

    Revert "Update imgui_stdlib.cpp"
    This reverts commit fcd5c92388ec5b48fc10b01c55a1ff447951ad0d.

--- thirdparty/imgui/imgui/misc/cpp/imgui_stdlib.cpp
+++ thirdparty/imgui/imgui/misc/cpp/imgui_stdlib.cpp
@@ -30,11 +30,9 @@ static int InputTextCallback(ImGuiInputTextCallbackData* data)
     }
     else if (user_data->ChainCallback)
     {
-        // Forward to user callback, if any
-        // else, just send InputTextCallback_UserData
-		if (user_data->ChainCallbackUserData) {
-			data->UserData = user_data->ChainCallbackUserData;
-		}
+        // DON'T Forward to user callback, if any
+        // Allows the callback to actually get the string
+        // data->UserData = user_data->ChainCallbackUserData;
         return user_data->ChainCallback(data);
     }
     return 0;
