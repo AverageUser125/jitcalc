6fb040f983f4c66cfe9c341dfbf6df46429784fd
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2024-10-17 19:40:42 +0300

    add glfwWindowHint for macos

--- src/gui/platform/mainGui.cpp
+++ src/gui/platform/mainGui.cpp
@@ -278,6 +278,12 @@ int guiLoop() {
 		return EXIT_FAILURE;
 	}
 	glfwWindowHint(GLFW_SAMPLES, 4);
+#ifdef __APPLE__ 
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, 1);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+#endif
 	wind = glfwCreateWindow(WINDOW_DEFAULT_WIDTH, WINDOW_DEFAULT_HEIGHT, "Graph calculator", nullptr, nullptr);
 	if (!wind) {
 		std::cerr << "Failed to create GLFW window" << std::endl;
