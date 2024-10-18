ec678d93791338aed565c6c9321a9b5fca3c9e88
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2024-10-18 11:54:08 +0300

    Update defines.hpp

--- include/defines.hpp
+++ include/defines.hpp
@@ -4,12 +4,19 @@
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
 #define PLATFORM_WIN 1
 #define PLATFORM_LINUX 0
+#define PLATFORM_MAC 0
 #elif defined(__linux__)
-#define PLATFORM_WINDOWS 0
+#define PLATFORM_WIN 0
 #define PLATFORM_LINUX 1
-#else
-#define PLATFORM_WINDOWS 0
+#define PLATFORM_MAC 0
+#elif defined(__APPLE__) && defined(__MACH__)
+#define PLATFORM_WIN 0
 #define PLATFORM_LINUX 0
+#define PLATFORM_MAC 1
+#else
+#define PLATFORM_WIN 0
+#define PLATFORM_LINUX 0
+#define PLATFORM_MAC 0
 #endif
 
 [[noreturn]] inline void unreachable() {
@@ -22,3 +29,15 @@
 	__builtin_unreachable();
 #endif
 }
+
+#if defined(__cplusplus)					// Check if compiling in C++
+#if defined(__GNUC__) || defined(__clang__) // For GCC/Clang
+#define RESTRICT __restrict__
+#elif defined(_MSC_VER) // For MSVC
+#define RESTRICT __restrict
+#else
+#define RESTRICT // Fallback if unknown C++ compiler
+#endif
+#else // For C
+#define RESTRICT restrict
+#endif
